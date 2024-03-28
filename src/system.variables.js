0483a295606a6bf14a429f663d5bf22961a9a286
dev-juanda01 <penalozahernandezjuandavid03@gmail.com>
2024-03-28 07:23:23 -0500

    Creacion de variables del sistema __dirname, __filename y __rootName

---
+++ src/system.variables.js
@@ -0,0 +1,9 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __rootDir = path.dirname(__filename);
+
+const __dirname = __rootDir;
+
+export { __dirname }
