307f0bef07975c4cc8da7ecd9d2b8da16cf462b0
dev-juanda01 <penalozahernandezjuandavid03@gmail.com>
2024-08-03 11:15:51 -0500

    Actualizacion en la gestion de eventos de socket

--- src/app/SocketServer.js
+++ src/app/SocketServer.js
@@ -1,117 +1,31 @@
-import { ClientService } from "../layers/application/ClientService.js";
-import { ConversationService } from "../layers/application/ConversationService.js";
-import { ClientRepository } from "../layers/domain/repositories/ClientRepository.js";
-import { ConversationRepository } from "../layers/domain/repositories/ConversationRepositorys.js";
-import * as constants from "./constants.js";
-
+/**
+ * Socket server for manage all events socket
+ */
 class SocketServer {
-    constructor(io, ws_manager, middlewares, socket_service) {
+    /**
+     *  Build a socket server class
+     * @param {{ io: Socket, ws_manager: WhatsAppManager, middlewares: SocketMiddlewares, socket_service: SocketService }} param0
+     */
+    constructor({ io, ws_manager, middlewares, socket_service }) {
         this.io = io;
         this.middlewares = middlewares;
         this.socket_service = socket_service;
         this.ws_manager = ws_manager;
-
-        // namespace of chat
-        this.chat_namespace = this.io
-            .of(constants.socket.namespaces.chats)
-            .use(this.middlewares.authenticate);
     }
 
-    chatEvents() {
-        this.chat_namespace.on(
-            constants.socket.events.connection,
-            async (socket) => {
-                console.log(constants.socket.logs.connected);
+    /**
+     * Initialize socket of all namespaces
+     */
+    initializeSocket() {}
 
-                // add user connected
-                this.socket_service.addUser({
-                    uuid: socket.uuid,
-                    data: {
-                        socket_id: socket.id,
-                        ...socket.user,
-                    },
-                });
-
-                console.log(this.socket_service.getUsers());
-
-                // disconnect
-                socket.on(constants.socket.events.disconnect, () => {
-                    console.log(constants.socket.logs.disconnected);
-                });
-
-                // list clients
-                const client_service = new ClientService(
-                    new ClientRepository()
-                );
-
-                const list_clients = await client_service.getClientsByUserRole(socket.uuid);
-                socket.emit(constants.socket.events.list_clients, list_clients);
-
-                // list conversations by user
-                const conversation_service = new ConversationService(
-                    new ConversationRepository(this.ws_manager)
-                );
-
-                socket.on(
-                    constants.socket.events.get_messages,
-                    async (data) => {
-                        let conversations = null;
-
-                        if (data) {
-
-                            conversations = await conversation_service.getConversationClient(socket.uuid, data.uuid);
-
-                            if (conversations.ok) {
-                                const client_chat = await client_service.getById(data.uuid);
-
-                                if (client_chat.ok) {
-                                    conversations.result = {
-                                        ...conversations.result,
-                                        client: {
-                                            uuid: client_chat.result.uuid,
-                                            name: client_chat.result.name,
-                                            phone: client_chat.result.phone,
-                                        },
-                                    };
-                                }
-                            }
-                        } else {
-                            conversations = {
-                                ok: false,
-                                status: constants.generals.code_status.STATUS_400,
-                                msg: constants.generals.messages.data_not_provider,
-                            };
-                        }
-
-                        socket.emit(
-                            constants.socket.events.list_messages,
-                            conversations
-                        );
-                    }
-                );
-
-                // received new message
-                socket.on(
-                    constants.socket.events.send_message,
-                    async (message) => {
-                        message.from = socket.uuid;
-
-                        let new_message = await conversation_service.create(
-                            message
-                        );
-
-                        if(new_message.result && new_message.result.messages) {
-                            new_message.result = new_message.result.messages[0]
-                        }
-
-                        socket.emit(
-                            constants.socket.events.received_message,
-                            new_message
-                        );
-                    }
-                );
-            }
-        );
+    /**
+     *  Create a socket instance to manage all events
+     * @param {{ io: Socket, ws_manager: WhatsAppManager, middlewares: SocketMiddlewares, socket_service: SocketService }} options properties neded to operational socket server
+     * @returns socket instance to manage events server
+     */
+    static createSocket(options) {
+        const socketInstance = new SocketServer(options);
+        socketInstance.initializeSocket();
     }
 }
 
